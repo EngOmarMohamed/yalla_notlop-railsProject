728a29ca2ca43bb8922c561afc9f7a2dc507893a
Eng Omar Mohamed <o.mohamed10@gmail.com>
2016-04-23 15:59:29 +0200

    modify in orders table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
 	  has_and_belongs_to_many :users , dependent: :destroy
-	    enum for: [ :breakfast, :dinner ]
+	    enum for: [ :breakfast, :launch ]
+	    enum status: [ :waiting, :finished ]
 
 
 end
--- db/migrate/20160421175449_create_orders.rb
+++ db/migrate/20160421175449_create_orders.rb
@@ -4,6 +4,7 @@ class CreateOrders < ActiveRecord::Migration
       t.string :image
       t.integer :for
       t.string :from
+      t.integer :status
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
